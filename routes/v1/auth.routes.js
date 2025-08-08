0b243dc36a875b347192f83b8805a6b420a0bc79
Ultivic User <ultivicuser@Ultivics-Mac-mini.local>
2025-08-08 16:25:58 +0530

    auth done

--- routes/v1/auth.routes.js
+++ routes/v1/auth.routes.js
@@ -12,6 +12,6 @@ router.post('/signup', signupValidator, authController.signup);
 router.post('/forgot-password', authController.forgotPassword);
 router.post('/reset-password', authController.resetPassword);
 router.get('/reset-password/:token', authController.renderPasswordSubmitPage);
-router.post('/google', authController.googleAuth);
+// router.post('/google', authController.googleAuth);
 
 module.exports = router;
