73d81f0ea8d8fbe4d0348e63ea3cd964b6ecbfcc
Ultivic User <ultivicuser@Ultivics-Mac-mini.local>
2025-08-13 13:28:12 +0530

    post module

--- service/user/auth.service.js
+++ service/user/auth.service.js
@@ -54,7 +54,7 @@ exports.login = async ({ email, password }) => {
     throw err;
   }
 
-  const token = await getJWT(email, user._id, user.role);
+  const token = await getJWT(email, user._id, "user");
   if (!token) {
     const err = new Error(resMessages.generalError.somethingWentWrong);
     err.statusCode = 400;
