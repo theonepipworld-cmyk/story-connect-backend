3cb167ae5a15680ba9369f9f75ff8359791fcccf
Ultivic User <ultivicuser@Ultivics-Mac-mini.local>
2025-08-14 17:02:32 +0530

    update err res

--- middlewares/requestValidations/user/profile.middleware.js
+++ middlewares/requestValidations/user/profile.middleware.js
@@ -12,7 +12,7 @@ const avatarUpload = multer({
   fileFilter: (req, file, cb) => {
     const allowedTypes = ['image/jpeg', 'image/png', 'image/webp', 'image/jpg'];
     if (!allowedTypes.includes(file.mimetype)) {
-      return cb(new Error('Only JPEG, PNG, and WEBP images are allowed'), false);
+      return cb(new Error('Only JPEG, PNG, JPG and WEBP images are allowed'), false);
     }
     cb(null, true);
   }
