2fc97103685c238ae5afe5c35de1a248ae5ec521
rahulMittal <rahul.mittal@ultivic.com>
2025-08-18 17:14:36 +0530

    seeding

--- config/db.js
+++ config/db.js
@@ -2,13 +2,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const mongoURI = process.env.MONGO_URI;
+console.log(mongoURI);
 
 async function connectDB() {  
     try {
-      await mongoose.connect(mongoURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      });
+      await mongoose.connect(mongoURI);
       console.log('Connected to MongoDB database.');
     } catch (err) {
       console.error('MongoDB connection failed:', err.message);
