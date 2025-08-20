9d52b256981f4c2a5731f915afed0996758ddb27
rahulMittal <rahul.mittal@ultivic.com>
2025-08-20 11:14:50 +0530

    changes

--- service/user/post.service.js
+++ service/user/post.service.js
@@ -2,31 +2,31 @@ const Post = require("../../models/post.model")
 const UserStats = require("../../models/userActivityStats.model")
 const mongoose = require("mongoose");
 const Comment = require("../../models/Comments.model")
-const { isPostExist, createError, postAggregationPipeline ,isUserExist } = require("../../helpers/dbHelpers.js")
+const { isPostExist, createError, postAggregationPipeline, isUserExist } = require("../../helpers/dbHelpers.js")
 const resMessages = require("../../constants/resMessages.constants.js")
 const Hashtag = require("../../models/hashTag.models.js")
 
 
 // Create Post
-exports.createPost = async (data,cleanHashTags) => {
+exports.createPost = async (data, cleanHashTags) => {
   const post = new Post(data);
   //update hashTagColection
   console.log(cleanHashTags)
-    if (cleanHashTags?.length > 0) {
-      await Promise.all(
-        cleanHashTags.map(async (tag) => {
-          await Hashtag.findOneAndUpdate(
-            { tag: tag },
-            {
-              $inc: { usageCount: 1 },
-              $addToSet: { posts: post._id } 
-            },
-            { upsert: true, new: true }
-          );
-        })
-      );
-    }
-  
+  if (cleanHashTags?.length > 0) {
+    await Promise.all(
+      cleanHashTags.map(async (tag) => {
+        await Hashtag.findOneAndUpdate(
+          { tag: tag },
+          {
+            $inc: { usageCount: 1 },
+            $addToSet: { posts: post._id }
+          },
+          { upsert: true, new: true }
+        );
+      })
+    );
+  }
+
   return await post.save();
 };
 
@@ -35,8 +35,8 @@ exports.getAllPosts = async (page, limit, search) => {
   const result = await Post.aggregate(postAggregationPipeline({}, page, limit, search));
   const data = result[0].data;
   const total = result[0].totalCount[0]?.count || 0;
- return {
-    posts: data,  
+  return {
+    posts: data,
     pagination: {
       total,
       totalPages: Math.ceil(total / limit),
@@ -49,12 +49,12 @@ exports.getAllPosts = async (page, limit, search) => {
 
 //get Single Post
 exports.getPostById = async (id) => {
-  const result =  await Post.aggregate(
-    postAggregationPipeline({ _id: new mongoose.Types.ObjectId(id) }, 1, 1 ,"")
+  const result = await Post.aggregate(
+    postAggregationPipeline({ _id: new mongoose.Types.ObjectId(id) }, 1, 1, "")
   );
-  const data =result[0].data;
+  const data = result[0].data;
   return {
-    post :data
+    post: data
   }
 };
 
@@ -115,34 +115,78 @@ exports.getProfilePost = async (id, page = 1, limit = 10) => {
     if (!user) {
       throw new Error('User not found');
     }
-
     const skip = (page - 1) * limit;
-
     const result = await Post.aggregate([
       { $match: { userId: new mongoose.Types.ObjectId(id) } },
       {
         $facet: {
           paginatedPosts: [
+            {
+              $lookup: {
+                from: "userstats",
+                localField: "_id",
+                foreignField: "postId",
+                as: "stats",
+              },
+            },
+            {
+              $addFields: {
+                totalLikes: { $ifNull: [{ $sum: "$stats.totalLikes" }, 0] },
+                totalViews: { $ifNull: [{ $sum: "$stats.totalViews" }, 0] },
+              },
+            },
+            {
+              $lookup: {
+                from: "comments",
+                localField: "_id",
+                foreignField: "postId",
+                as: "comments",
+              },
+            },
+            {
+              $addFields: {
+                totalComments: { $size: "$comments" },
+              },
+            },
+                 {
+            $project: {
+              _id: 1,
+              postHeading: 1,
+              postDescription: 1,
+              mediaUrls: 1,
+              hashtags: 1,
+              communityId: 1,
+              type: 1,
+              storyOfTheMonth: 1,
+              videoOfTheMonth: 1,
+              createdAt: 1,
+              updatedAt: 1,
+              totalLikes: 1,
+              totalViews: 1,
+              totalComments: 1,
+            },
+          },
             { $sort: { createdAt: -1 } },
             { $skip: skip },
-            { $limit: limit }
+            { $limit: limit },
           ],
           totalCount: [
             { $count: "count" }
-          ]
-        }
-      }
+          ],
+        },
+      },
     ]);
-
     const posts = result[0].paginatedPosts;
     const totalPosts = result[0].totalCount[0] ? result[0].totalCount[0].count : 0;
 
-    return { posts, pagination: {
-      totalPosts,
-      totalPages: Math.ceil(totalPosts / limit),
-      currentPage: parseInt(page),
-      limit: parseInt(limit),
-    }, };
+    return {
+      posts, pagination: {
+        totalPosts,
+        totalPages: Math.ceil(totalPosts / limit),
+        currentPage: parseInt(page),
+        limit: parseInt(limit),
+      },
+    };
   } catch (error) {
     throw new Error(error.message);
   }
