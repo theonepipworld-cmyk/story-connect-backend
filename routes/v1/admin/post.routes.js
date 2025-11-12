24d0f2e8002b5fd4110d148e8bb0535931e999e1
Tarun123-kittu <tarun.gautam@ultivic.com>
2025-11-12 17:26:02 +0530

    create publi route

--- routes/v1/admin/post.routes.js
+++ routes/v1/admin/post.routes.js
@@ -8,5 +8,4 @@ const { authorizeRoles } = require('../../../middlewares/requestValidations/user
 
 router.put("/add-highlighted-post", isAuthenticated, authorizeRoles('admin'), adminpostController.addStoryAndVideoOfMonth);
 router.put("/remove-highlighted-post", isAuthenticated, authorizeRoles('admin'), adminpostController.removeStoryAndVideoOfMonth);
-router.get("/highlightedPost", adminpostController.getHighlightedPosts)
 module.exports = router;
