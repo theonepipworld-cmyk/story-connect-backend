3d6b93621d42d1d9d20f65a6273310254f829f60
Tarun123-kittu <tarun.gautam@ultivic.com>
2026-01-21 18:13:26 +0530

    changes

--- service/admin/authService.js
+++ service/admin/authService.js
@@ -41,7 +41,7 @@ exports.forgotPassword = async ({ email }) => {
         user.resetPasswordExpires = Date.now() + 1000 * 60 * 15;
         await user.save();
 
-        const resetLink = `${ADMIN_RESET_PASS_LINK}/api/v1/auth/reset-password?token=${resetToken}`;
+        const resetLink = `${ADMIN_RESET_PASS_LINK}/reset-password?token=${resetToken}`;
 
         await sendEmail({
             to: email,
