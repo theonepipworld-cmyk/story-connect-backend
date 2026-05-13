71e0e1aec50319052fb0a3759fe8ddf9d6951737
akshita-tomar <122252793+akshita-tomar@users.noreply.github.com>
2026-05-13 18:30:17 +0530

    added inactive on each api check

--- middlewares/requestValidations/user/authorizeRoles.js
+++ middlewares/requestValidations/user/authorizeRoles.js
@@ -2,33 +2,43 @@
 
 const { errorResponse } = require("../../../utils/responseHandler.util");
 const resMessages = require("../../../constants/resMessages.constants");
+const User = require("../../../models/user.model");
+
+
+const checkUserDetails = async (userId) => {
+  const user = await User.findById(userId);
+  return user;
+};
 
 exports.authorizeRoles = (...allowedRoles) => {
-  return (req, res, next) => {
+  return async (req, res, next) => { 
+    try {
+     
+      if (!req.user) {
+        return res.status(401).json(errorResponse(resMessages.auth.unauthorizedAccess));
+      }
 
-    // Check if user exists
-    if (!req.user) {
-      return res
-        .status(401)
-        .json(errorResponse(resMessages.auth.unauthorizedAccess));
+  
+      const userDetails = await checkUserDetails(req.user.id);
+
+
+      if (!userDetails) {
+        return res.status(401).json(errorResponse(resMessages.auth.unauthorizedAccess));
+      }
+
+      if (userDetails.status !== "active") {
+        return res.status(403).json(errorResponse("Your account is inactive. Please contact admin."));
+      }
+
+  
+      if (!allowedRoles.includes(userDetails.role)) {
+        return res.status(403).json(errorResponse(resMessages.auth.unauthorizedAccess));
+      }
+
+      next();
+    } catch (error) {
+      console.error("Error in authorizeRoles middleware:", error);
+      return res.status(500) .json(errorResponse("Something went wrong. Please try again later.", error.message));
     }
-    
-
-    // Check if user is active
-    console.log("User status in authorizeRoles middleware:", req.user);
-    if (req.user.status !== 'active') {
-      return res
-        .status(403)
-        .json(errorResponse("Your account is inactive. Please contact admin."));
-    }
-
-    // Check role authorization
-    if (!allowedRoles.includes(req.user.role)) {
-      return res
-        .status(403)
-        .json(errorResponse(resMessages.auth.unauthorizedAccess));
-    }
-
-    next();
   };
 };
